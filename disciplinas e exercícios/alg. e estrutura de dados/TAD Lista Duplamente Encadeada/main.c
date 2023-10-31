6d87925aa6d0a5253f581850f4e3af7fb60ff692
jjuniorbrasil <juniorbrasilisc@gmail.com>
2023-10-31 11:13:38 -0300

    Mudanças breves

--- disciplinas e exercícios/alg. e estrutura de dados/TAD Lista Duplamente Encadeada/main.c
+++ disciplinas e exercícios/alg. e estrutura de dados/TAD Lista Duplamente Encadeada/main.c
@@ -4,37 +4,24 @@
 #include "ListaDupla.h"
 #include <time.h>
 #include <locale.h>
+#include "FilaDupla.h"
 
-Lista* minha_lista;
+Fila* minha_fila;
 
 struct aluno al;
 
 int main()
 {
-    int n;
-    minha_lista = criar_lista();
-    printf("LISTA DUPLAMENTE ENCADEADA: Lista criada! \n");
+    minha_fila = criar_lista();
 
-    setlocale(LC_ALL, "Portuguese");
-    clock_t t;
-    t = clock();
-
-    for (int x = 0; x < 500000; x++)
-    {
     al.matricula = x;
     strcpy(al.nome,"Teste");
     al.n1 = 7.5;
     al.n2 = 8.2;
     al.n3 = 7.4;
-    inserir_lista_inicio(minha_lista,al);
-    }
+    inserir_lista_inicio(minha_fila,al);
 
-    liberar_lista(minha_lista);
+    liberar_lista(minha_fila);
 
     printf("A lista foi liberada!");
-
-    t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-    printf("\n\n'500000' inserções em %f segundos. \n", time_taken);
-    scanf("%d", &n);
 }
